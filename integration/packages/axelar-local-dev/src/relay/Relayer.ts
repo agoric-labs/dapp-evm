81a5f598a938bcb7cf318bf35dd360ec58166815
rabi-siddique <rabisiddique27@gmail.com>
2025-04-09 16:26:57 +0500

    feat: setup neccessary packages for axelar

---
+++ integration/packages/axelar-local-dev/src/relay/Relayer.ts
@@ -0,0 +1,65 @@
+import { networks, Network } from '../Network';
+import { Command } from './Command';
+import { CallContractArgs, CallContractWithTokenArgs, RelayCommand, RelayData } from './types';
+
+export enum RelayerType {
+    Sui = 'sui',
+    Evm = 'evm',
+    Aptos = 'aptos',
+    Near = 'near',
+    Wasm = 'wasm',
+    Agoric = 'agoric',
+    MultiversX = 'multiversx',
+}
+export type RelayerMap = Partial<Record<RelayerType, Relayer>> & { [key: string]: Relayer | undefined };
+
+export abstract class Relayer {
+    public otherRelayers: RelayerMap = {};
+    public relayData: RelayData = {
+        depositAddress: {},
+        sendToken: {},
+        callContract: {},
+        callContractWithToken: {},
+    };
+
+    protected commands: RelayCommand = {};
+    public contractCallGasEvents: any[] = [];
+    public contractCallWithTokenGasEvents: any[] = [];
+    public expressContractCallGasEvents: any[] = [];
+    public expressContractCallWithTokenGasEvents: any[] = [];
+    abstract updateEvents(): Promise<void>;
+    abstract execute(commands: RelayCommand): Promise<void>;
+    abstract createCallContractCommand(commandId: string, relayData: RelayData, contractCallArgs: CallContractArgs): Command;
+    abstract createCallContractWithTokenCommand(
+        commandId: string,
+        relayData: RelayData,
+        callContractWithTokenArgs: CallContractWithTokenArgs
+    ): Command;
+
+    abstract setRelayer(type: RelayerType, relayer: Relayer): void;
+
+    async relay(externalNetworks?: Network[]) {
+        const actualNetworks = externalNetworks || networks;
+        for (const to of actualNetworks) {
+            this.commands[to.name] = [];
+        }
+        this.commands['aptos'] = [];
+        this.commands['sui'] = [];
+        this.commands['near'] = [];
+        this.commands['wasm'] = [];
+        this.commands['agoric'] = [];
+        this.commands['multiversx'] = [];
+        // Update all events at the source chains
+        await this.updateEvents();
+
+        await this.execute(this.commands);
+    }
+
+    async subscribeExpressCall() {
+        // this is a no-op by default
+    }
+
+    unsubscribe() {
+        // this is a no-op by default
+    }
+}
