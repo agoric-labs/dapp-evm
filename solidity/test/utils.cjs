ba959bc18059bfe913d9c69504c4c031cdcd0161
rabi-siddique <rabisiddique27@gmail.com>
2025-04-25 12:21:21 +0500

    test: fix failing solidity tests and minor improvements

---
+++ solidity/test/utils.cjs
@@ -0,0 +1,26 @@
+// @ts-check
+const { encodeFunctionData } = require('viem');
+
+const constructContractCall = ({ target, functionSignature, args }) => {
+  const [name, paramsRaw] = functionSignature.split('(');
+  const params = paramsRaw.replace(')', '').split(',').filter(Boolean);
+
+  return {
+    target,
+    data: encodeFunctionData({
+      abi: [
+        {
+          type: 'function',
+          name,
+          inputs: params.map((type, i) => ({ type, name: `arg${i}` })),
+        },
+      ],
+      functionName: name,
+      args,
+    }),
+  };
+};
+
+module.exports = {
+  constructContractCall,
+};
