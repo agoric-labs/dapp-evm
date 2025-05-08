60ac0b2a96f04913125e89ac2a8c479331c9362a
rabi-siddique <rabisiddique27@gmail.com>
2025-05-08 12:41:51 +0500

    chore(style): remove redundant ui styles in content container

--- ui/src/state.ts
+++ ui/src/state.ts
@@ -4,7 +4,7 @@ import { create } from 'zustand';
 export const useAppStore = create<AppState>((set) => ({
   contractInstance: null,
   evmAddress: '0x',
-  destinationEVMChain: 'Avalanche',
+  destinationEVMChain: 'Ethereum',
   amountToSend: 0,
   loading: false,
   error: undefined,
